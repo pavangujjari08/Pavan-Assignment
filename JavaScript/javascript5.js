a3550f1903fa48b6a76a159095708d1b5c688471
pavan gujjari <pavan.gujjari@celigo.com>
2022-04-29 16:19:10 +0530

    The Sorting of Json Object is Done.

--- JavaScript/javascript5.js
+++ JavaScript/javascript5.js
@@ -6,15 +6,33 @@ class SortArray {
     return this._orginalArray.sort();
   }
 }
-class SortObjectArray extends SortArray{
-  constructor(orginalArray,model){
-    super(orginalArray);
-    this._model=model
-  }
-  getModel(){
-    return this._model;
+class SortObjectArray extends SortArray
+  {
+    constructor(orginalArray,arrayObject)
+      {
+        super(orginalArray);
+        this._arrayObject=arrayObject;
+      }
+
+  getSortObjectArray()
+  {
+    this._arrayObject.sort(function (a, b)
+                           {
+                              return a.value - b.value;
+                            }
+                          );
+    return this._arrayObject;
   }
 }
 let A = [7,2,4,1];
-const objArr = new SortObjectArray(A,'Quick');
+let items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic', value: 13 },
+  { name: 'Zeros', value: 37 }
+];
+const objArr = new SortObjectArray(A,items);
 console.log(objArr.getSortedArray());
+console.log(objArr.getSortObjectArray());
